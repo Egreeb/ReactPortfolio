7d8723a917a4d7c89c5010eabe516882afc5ee27
Egreebai <164660834+Egreebai@users.noreply.github.com>
2025-11-19 09:26:59 +0400

    My Portfolio

---
+++ Controller/Email.js
@@ -0,0 +1,46 @@
+import nodemailer from 'nodemailer'
+import dotenv from 'dotenv'
+dotenv.config()
+
+export const Email = async(req,res)=>{
+     const {fullname, email, message, number,subject} = req.body
+    //  console.log(fullname, email, message)
+    try {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth : {
+                user: process.env.ADMIN_EMAIL,
+                pass: process.env.PASSWORD,
+            }
+        });
+
+        await transporter.sendMail({
+            from: process.env.ADMIN_EMAIL,
+            to: process.env.ADMIN_EMAIL,
+            subject: "New Contact From Message",
+            html:  
+            `
+                <h3>New Message Received</h3>
+                <p><b>Name:</b> ${fullname}</p>
+                <p><b>Email:</b> ${email}</p>
+                <p><b>Number:</b> ${number}</p>
+                <p><b>Subject:</b> ${subject}</p>
+                <p><b>Message:</b> ${message}</p>
+            `
+        })
+        await transporter.sendMail({
+            from: process.env.ADMIN_EMAIL,
+            to: email,
+            subject:"Thank You for Contacting Me!",
+            html: 
+                `
+                <h3>Hello ${fullname},</h3>
+                <p>Thank you for your message. I will get back to you shortly.</p>
+                `
+        });
+         res.json({ success: true, message: "Emails sent successfully!"});
+
+    } catch (error) {
+        console.error(error)
+    }
+}
---
+++ Server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import { Email } from './Controller/Email.js'
+import cors from 'cors'
+const app = express()
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+app.use(cors({
+    origin:true,
+    methods:["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}))
+app.use(express.json())
+
+app.post('/', Email)
+
+
+app.listen( process.env.PORT, ()=> console.log(`server is running on ${process.env.PORT}`))
